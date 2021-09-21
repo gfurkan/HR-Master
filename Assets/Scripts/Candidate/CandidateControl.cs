3c20be01acc8da53ff1662ffe6a7ffdbee4fcf0f
furkan <34320035+gfurkan@users.noreply.github.com>
2021-09-21 14:56:21 +0300

    Project Finished.

--- Assets/Scripts/Candidate/CandidateControl.cs
+++ Assets/Scripts/Candidate/CandidateControl.cs
@@ -2,35 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class CandidateControl : MonoBehaviour
 {
     #region Arrays
     [SerializeField]
-    private static string[] names= {"John","Josh","Neil","Tom","Tommy","Sam","Thomas","Carter","Max","Luis","Samuel"};
+    private static string[] _names= {"John","Josh","Neil","Tom","Tommy","Sam","Thomas","Carter","Max","Luis","Samuel"};
     [SerializeField]
-    private static string[] departments = { "IT", "Developer", "Artist", "Analyst", "Growth Manager", "CEO", "CTO",};
+    private static string[] _departments = {"Developer", "Artist", "Analyst"};
     #endregion
 
     #region Fields
     Animator animator;
     NavMeshAgent agent;
 
-    private static Transform chair, door;
+    private static Text nameText, salaryText, departmentText, experienceText;
+
+    private static int _salaryLimit=10, _experienceLimit = 10;
+    private static PlayerControl playerControl;
+
+    private static Transform sittingPos, door;
     private static Vector3 chairPos, doorPos;
-    private string _name, _department;
+    private string _candidateName, _department;
 
     private int _experience, _salary;
     private bool _goToChair = false, _goToDoor = false;
     #endregion
 
     #region Properties
-    public string name => _name;
+    public string candidateName => _candidateName;
     public string department => _department;
 
     public int experience => _experience;
     public int salary => _salary;
+    public int salaryLimit => _salaryLimit;
+    public int experienceLimit => _experienceLimit;
 
+    public string[] departments => _departments;
     public bool goToChair
     {
         get
@@ -57,44 +67,50 @@ public class CandidateControl : MonoBehaviour
 
     void Start()
     {
-        chair = GameObject.FindGameObjectWithTag("ChairPos").transform;
-        door = GameObject.FindGameObjectWithTag("Door").transform;
+        GetReferences();
+        SetCandidate›nfos();
 
         animator = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
 
-        _name = names[Random.Range(0, names.Length)];
-        _salary = Random.Range(3, 15)*1000;
-        _department=departments[Random.Range(0, departments.Length)];
-        _experience = Random.Range(0, 20);
-
-        chairPos = new Vector3(chair.position.x, transform.position.y, chair.position.z);
+        chairPos = new Vector3(sittingPos.position.x, transform.position.y, sittingPos.position.z);
         doorPos= new Vector3(door.position.x, transform.position.y, door.position.z);
     }
 
-    private void Update()
+    void GetReferences()
     {
-        if (_goToChair)
-        {
-            GoToChair();
-        }
-        if (_goToDoor)
-        {
-            GoToDoor();
-        }
+        nameText = GameObject.FindGameObjectWithTag("NameText").GetComponent<Text>();
+        salaryText = GameObject.FindGameObjectWithTag("SalaryText").GetComponent<Text>();
+        departmentText = GameObject.FindGameObjectWithTag("DepartmentText").GetComponent<Text>();
+        experienceText = GameObject.FindGameObjectWithTag("ExperienceText").GetComponent<Text>();
+
+        playerControl = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControl>();
+        sittingPos = GameObject.FindGameObjectWithTag("Player").transform;
+        door = GameObject.FindGameObjectWithTag("Door").transform;
     }
-    void GoToChair()
+
+    void SetCandidate›nfos()
+    {
+
+        _candidateName = _names[Random.Range(0, _names.Length)];
+        _salary = Random.Range(3, 15) * 1000;
+        _department = _departments[Random.Range(0, _departments.Length)];
+        _experience = Random.Range(0, 20);
+    }
+
+    public void GoToChair()
     {
         agent.enabled = true;
         agent.SetDestination(chairPos);
         animator.SetBool("Walk", true);
 
     }
-    void GoToDoor()
+
+    public void GoToDoor()
     {
         agent.enabled = true;
-        animator.SetBool("Walk", true);
         agent.SetDestination(doorPos);
+        playerControl.ChangeCandidate();
     }
     public void ElectionResult (bool eliminated)
     {
@@ -109,4 +125,19 @@ public class CandidateControl : MonoBehaviour
             animator.SetBool("Walk", true);
         }
     }
+    void ShowCandidatePanel() //Called from sitting animation event.
+    {
+        nameText.text = "Name : " + _candidateName;
+        salaryText.text = "Salary : " + _salary.ToString() + "Ä";
+        departmentText.text = "Department : " + _department;
+        experienceText.text = "Experience : " + _experience.ToString() + " years";
+
+        salaryText.transform.parent.gameObject.GetComponent<CanvasGroup>().DOFade(1, 0.5f);
+    }
+    void HideCandidatePanel() //Called from clapping and disbelief animation events.
+    {
+        salaryText.transform.parent.gameObject.GetComponent<CanvasGroup>().DOFade(0,0.5f);
+        
+    }
+
 }
--- Assets/Scripts/Player/PlayerControl.cs
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -3,24 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class PlayerControl : MonoBehaviour
 {
+    #region Fields
     [SerializeField]
     private GameObject candidatePrefab;
     [SerializeField]
     private int candidateCount;
     [SerializeField]
-    private Text salaryText, departmentText, experienceText;
+    private Text salaryExpectationText, departmentExpectationText, experienceExpectationText, totalCandidatesText, trueDecisionsText, falseDecisionText;
+    [SerializeField]
+    private GameObject levelFinishPanel;
+    [SerializeField]
+    private Button startGameButton;
 
     private List<GameObject> candidates = new List<GameObject>();
-    private int candidateNumber = 0;
+    private int candidateNumber = 0, trueDecisions = 0, falseDecisions = 0, salaryExpectation, experienceExpectation;
+    private string departmentExpectation;
+    private float candidateZPos = -2;
 
-    private float candidateZPos=-2;
     InputManager inputManager;
     CandidateControl candidateControl;
 
     private bool canDrag = false;
+
+    #endregion
 
     void Start()
     {
@@ -31,8 +40,7 @@ public class PlayerControl : MonoBehaviour
             candidateZPos += 2;
             candidates.Add(cand);
         }
-        candidateControl = candidates[candidateNumber].GetComponent<CandidateControl>();
-        candidateControl.goToChair = true;
+
     }
 
     void Update()
@@ -47,54 +55,57 @@ public class PlayerControl : MonoBehaviour
         }
     }
 
-    void ChangeCandidate()
+    public void ChangeCandidate()
     {
         if (candidateNumber+1 != candidateCount) 
         {
             candidateNumber++;
             candidateControl = candidates[candidateNumber].GetComponent<CandidateControl>();
-            candidateControl.goToChair = true;
+            candidateControl.GoToChair();
         }
-        salaryText.text = "Salary : ";
-        departmentText.text = "Department : ";
-        experienceText.text = "Experience : ";
+        else
+        {
+            LevelFinish();
+            LevelManager.Instance.LevelCompleted();
+        }
     }
 
     void MakeElection()
     {
+
         if (inputManager.directionVec.x > 0)
         {
             candidateControl.ElectionResult(false);
-            float waitingTime = 3f;
-            StartCoroutine("GoToDoor",waitingTime);
-
             canDrag = false;
 
+            if (candidateControl.salary <= salaryExpectation && candidateControl.department == departmentExpectation && candidateControl.experience >= experienceExpectation)
+            {
+                trueDecisions++;
+            }
+            else
+            {
+                falseDecisions++;
+            }
         }
         if (inputManager.directionVec.x < 0)
         {
             candidateControl.ElectionResult(true);
-            float waitingTime = 4f;
-            StartCoroutine("GoToDoor", waitingTime);
-
             canDrag = false;
-        }
-    }
 
-    IEnumerator GoToDoor(float time)
-    {
-        yield return new WaitForSeconds(time);
-        candidateControl.goToDoor = true;
-        ChangeCandidate();
+            if (candidateControl.salary > salaryExpectation || candidateControl.department != departmentExpectation || candidateControl.experience < experienceExpectation)
+            {
+                trueDecisions++;
+            }
+            else
+            {
+                falseDecisions++;
+            }
+        }
     }
 
     IEnumerator CanDrag()
     {
         yield return new WaitForSeconds(1);
-        salaryText.text ="Salary : "+ candidateControl.salary.ToString() + "€";
-        departmentText.text ="Department : " +candidateControl.department;
-        experienceText.text ="Experience : "+ candidateControl.experience.ToString() + " years";
-
         canDrag = true;
     }
 
@@ -102,16 +113,40 @@ public class PlayerControl : MonoBehaviour
     {
          if (other.gameObject.tag == "Candidate")
          {
-            candidateControl.goToChair = false;
             other.transform.rotation = Quaternion.Euler(0,180,0);
 
             other.GetComponent<NavMeshAgent>().enabled = false;
             other.GetComponent<Animator>().SetBool("Sit", true);
 
             StartCoroutine("CanDrag");
-
             other.GetComponent<Collider>().enabled = false;
          }
     }
+    void SetExpectations()
+    {
+        salaryExpectation = Random.Range(5, candidateControl.salaryLimit)*1000;
+        experienceExpectation = Random.Range(0, candidateControl.experienceLimit);
+        departmentExpectation = candidateControl.departments[Random.Range(0, candidateControl.departments.Length)];
 
+
+        salaryExpectationText.text = "Salary <= " + salaryExpectation + "€";
+        departmentExpectationText.text = "Department : " + departmentExpectation;
+        experienceExpectationText.text = "Experience => " + experienceExpectation + " yrs.";
+    }
+    void LevelFinish()
+    {
+        totalCandidatesText.text = "Total Candidates : " + candidateCount;
+        trueDecisionsText.text = "True Decisions : " + trueDecisions;
+        falseDecisionText.text = "False Decisions : " + falseDecisions;
+
+        levelFinishPanel.GetComponent<CanvasGroup>().DOFade(1, 0.5f);
+    }
+
+    public void StartGameButton()
+    {
+        candidateControl = candidates[candidateNumber].GetComponent<CandidateControl>();
+        candidateControl.GoToChair();
+        SetExpectations();
+        startGameButton.GetComponent<CanvasGroup>().DOFade(0, 0.25f);
+    }
 }
